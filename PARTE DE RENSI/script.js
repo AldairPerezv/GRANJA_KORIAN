06471887878a024e5f0a6800c59956e0d6a99ef8
AldairPerezv <129784268+AldairPerezv@users.noreply.github.com>
2023-08-17 18:23:45 -0500

    baner80%

--- PARTE DE RENSI/script.js
+++ PARTE DE RENSI/script.js
@@ -12,3 +12,31 @@ var swiper = new Swiper(".mySwiper-1", {
     }
     
 })
+
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView:3,
+    spaceBetween: 20,
+    loop:true,
+    loopFillGroupWithBlank:true,
+    navigation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    },
+    breakpoints : {
+        0: {
+            sliderPerView:1,
+        }
+    }
+});
+
+let tabInputs = document.querySelectorAll(".tabInput");
+
+tabInputs.forEach(function(input) {
+
+        input.addEventListener('change', function() {
+            let id = input.ariaValueMax;
+            let thisSwiper = document.getElementById('swiper' + id);
+            thisSwiper.swiper.update();
+        })
+
+});
